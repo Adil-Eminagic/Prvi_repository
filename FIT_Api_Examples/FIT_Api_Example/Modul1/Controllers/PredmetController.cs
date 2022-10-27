b9121fb041581f280fe235548bff3f5f306e6652
adil <adil@example.com>
2022-10-27 10:00:28 +0200

    predzadnje za komit v3

---
+++ FIT_Api_Examples/FIT_Api_Example/Modul1/Controllers/PredmetController.cs
@@ -0,0 +1,50 @@
+﻿using FIT_Api_Example.Data;
+using FIT_Api_Example.Helper;
+using FIT_Api_Example.Modul1.Models;
+using FIT_Api_Example.Modul1.ViewModels;
+using FIT_Api_Example.Modul2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT_Api_Example.Modul2.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PredmetController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PredmetController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+     
+        [HttpPost]
+        public Predmet Add([FromBody] DrzavaAddVM x)
+        {
+            var noviZapis = new Predmet();
+            {
+               
+            };
+
+            _dbContext.Add(newEmployee);
+            _dbContext.SaveChanges();
+            return newEmployee;
+        }
+
+        [HttpGet]
+        public List<CmbStavke> GetAll()
+        {
+            var data = _dbContext.Drzava
+                .OrderBy(s => s.naziv)
+                .Select(s => new CmbStavke()
+                {
+                    id = s.id,
+                    opis = s.naziv,
+                })
+                .AsQueryable();
+            return data.Take(100).ToList();
+        }
+    }
+}
