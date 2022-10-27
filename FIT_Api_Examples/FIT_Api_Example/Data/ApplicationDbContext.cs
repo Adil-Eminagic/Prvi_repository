ec3ae2d7e36f2a1a8207a686d7c9c1215b1227d6
adil <adil@example.com>
2022-10-27 08:53:35 +0200

    zadatak dodan

---
+++ FIT_Api_Examples/FIT_Api_Example/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FIT_Api_Example.Modul1.Models;
+using FIT_Api_Example.Modul2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIT_Api_Example.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Drzava> Drzava { get; set; }
+        public DbSet<Opstina> Opstina { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Predmet>  Predmet { get; set; }
+        public DbSet<Ispit> Ispit { get; set; }
+
+        public ApplicationDbContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
