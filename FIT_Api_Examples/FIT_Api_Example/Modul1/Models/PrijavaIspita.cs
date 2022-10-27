5cf3da4d7cdd77ee98f271fd6e5a4f6ab291b17d
adil <adil@example.com>
2022-10-27 09:07:20 +0200

    prvi comit proba v3

---
+++ FIT_Api_Examples/FIT_Api_Example/Modul1/Models/PrijavaIspita.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIT_Api_Example.Modul1.Models
+{
+    public class PrijavaIspita
+    {
+        [Key]
+        public int ID { get; set; }
+        public DateTime DatumPrijave { get; set; }
+
+    }
+}
